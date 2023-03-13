10d538916741c3d368640fdd072d9ec00fd80a55
Glowman554 <glowman434@gmail.com>
2023-03-13 10:25:19 +0100

    actually compile

--- scripts/test_firestorm.sh
+++ scripts/test_firestorm.sh
@@ -2,13 +2,14 @@ set -ex
 
 function compile {
 	./brainasm.elf -t any-any-firestorm -o $2 $1
+	flc $2 -o $3
 }
 
 make
 
 mkdir -p out
 
-compile test/hello.bf out/hello.fl
-compile test/yapi.bf out/yapi.fl
-compile test/cellsize.bf out/cellsize.fl
-compile test/triangle.bf out/triangle.fl
+compile test/hello.bf out/hello.fl out/hello.elf
+compile test/yapi.bf out/yapi.fl out/yapi.elf
+compile test/cellsize.bf out/cellsize.fl out/cellsize.elf
+compile test/triangle.bf out/triangle.fl out/triangle.elf
